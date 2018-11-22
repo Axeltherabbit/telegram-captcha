36e7b562b2187dea28f9673234fb3d5b073ab2da
axeltherabbit <backb0x@hotmail.com>
2018-11-22 17:24:38 +0000

    unmute bug fix

--- buttons_query.py
+++ buttons_query.py
@@ -8,7 +8,10 @@ def buttons(bot, update):
             r.setex(f"aweek:{query.from_user.id}",604800,'')
             print(f"{query.from_user.id} {query.from_user.first_name} @{query.from_user.username} has solved the captcha")
             try:
-                bot.restrictChatMember(query.message.chat_id,query.from_user.id,can_send_messages=True)
+                bot.restrictChatMember(query.message.chat_id,query.from_user.id,can_send_messages=True,
+                                                                                can_send_media_messages=True,
+                                                                                can_send_other_messages=True,
+                                                                                can_add_web_page_previews=True)
             except Exception as e:
                 print(e)
     else:
