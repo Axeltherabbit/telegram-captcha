025130e9c28192c898963c103df67c9fe19a4ad7
axeltherabbit <backb0x@hotmail.com>
2018-11-26 12:51:33 +0000

    permissions bug fix

--- expired_records.py
+++ expired_records.py
@@ -8,20 +8,24 @@ def expiration_listener(pubsub, bot):
             user_id=data[1]
             chat_id=data[2]
             message_id=data[3]
-            bot.kickChatMember(chat_id=chat_id,user_id=user_id)
-
-            if r.get(f"warn:{user_id}") is None :
-                r.setex(f"warn:{user_id}",86400,1)
-            else:
-                r.setex(f"warn:{user_id}",86400,int(r.get(f"warn:{user_id}"))+1)
-            num_warn=int(r.get(f"warn:{user_id}"))
-            bot.editMessageText(chat_id=chat_id,message_id=message_id,text=f"captcha failed, the user has been kicked, warn: {num_warn}/3")
-            print(f"{user_id} has been banned from {chat_id} -> warn num {num_warn}")
-            if num_warn < 3:
-                try:#just for supergroups
-                    bot.unbanChatMember(chat_id=chat_id,user_id=user_id)
-                except:
-                    pass
-            else:
-                bot.sendMessage(chat_id,"warn number 3, permanent ban")
-                print(f"permanent ban from {chat_id} for {user_id}")
+            
+            try:#it need admin permissions
+                bot.kickChatMember(chat_id=chat_id,user_id=user_id)
+            except Exception as e:
+                print(e)
+            else:#if it has admin permissions
+                if r.get(f"warn:{user_id}") is None :
+                    r.setex(f"warn:{user_id}",86400,1)
+                else:
+                    r.setex(f"warn:{user_id}",86400,int(r.get(f"warn:{user_id}"))+1)
+                num_warn=int(r.get(f"warn:{user_id}"))
+                bot.editMessageText(chat_id=chat_id,message_id=message_id,text=f"captcha failed, the user has been kicked, warn: {num_warn}/3")
+                print(f"{user_id} has been banned from {chat_id} -> warn num {num_warn}")
+                if num_warn < 3:
+                    try:#just for supergroups
+                        bot.unbanChatMember(chat_id=chat_id,user_id=user_id)
+                    except Exception as e:
+                        print(e)
+                else:
+                    bot.sendMessage(chat_id,"warn number 3, permanent ban")
+                    print(f"permanent ban from {chat_id} for {user_id}")
