3e37f157ae4dc0c652bf4a57aa490b41135e6bed
PabloWong <47540797+PabloWong@users.noreply.github.com>
2019-02-18 11:30:33 -0400

    Update buttons_query.py

--- buttons_query.py
+++ buttons_query.py
@@ -5,7 +5,7 @@ def buttons(bot, update):
     query = update.callback_query
     if query.data == str(query.from_user.id):
         if r.delete(f"aminute:{query.from_user.id}:{query.message.chat_id}:{query.message.message_id}") > 0:
-            bot.editMessageText(chat_id=query.message.chat_id,message_id=query.message.message_id,text="captcha solved ✅")
+            bot.deleteMessage(chat_id=query.message.chat_id,message_id=query.message.message_id)
             r.setex(f"aweek:{query.from_user.id}",604800,'')
             log.debug(f"{query.from_user.id} {query.from_user.first_name} @{query.from_user.username} has solved the captcha")
             try:
@@ -16,4 +16,5 @@ def buttons(bot, update):
             except Exception as e:
                 log.warning(e)
     else:
-        bot.answer_callback_query(query.id, text="It's not for you", show_alert=True)
+        pass
+        #bot.answer_callback_query(query.id, text="It's not for you", show_alert=True)
